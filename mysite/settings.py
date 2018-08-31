ee5b4df8b38a80b7a7c867fab073ca88ac1b7698
Juliano Bordignon <bordignonjm@gmail.com>
2018-08-31 14:30:06 -0300

    teste envio

--- mysite/settings.py
+++ mysite/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'm09(z$gu$et=#!9mrgsxru1b55g)=_4c%f%1-4xmx@pr&ofcbo'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 
